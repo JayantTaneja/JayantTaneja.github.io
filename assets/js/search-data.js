d098fc21f4cb42a197b9ed4516a721cd330842ea
JayantTaneja <79043865+JayantTaneja@users.noreply.github.com>
2025-11-05 11:03:28 +0000

    Deploying to gh-pages from @ JayantTaneja/JayantTaneja.github.io@693c54845c5ef4bdcede7f1b2401752e52d4cf8c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -4,20 +4,20 @@ const ninja = document.querySelector('ninja-keys');
 // add the home and posts menu items
 ninja.data = [{
     id: "nav-about",
-    title: "about",
+    title: "About",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
     },
   },{id: "nav-publications",
-          title: "publications",
+          title: "Publications",
           description: "Some selected works",
           section: "Navigation",
           handler: () => {
             window.location.href = "/publications/";
           },
         },{id: "nav-cv",
-          title: "cv",
+          title: "CV",
           description: "The Journey so far...",
           section: "Navigation",
           handler: () => {
@@ -438,6 +438,13 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("https://scholar.google.com/citations?user=XoCqkOUAAAAJ", "_blank");
+        },
+      },{
+        id: 'social-github',
+        title: 'GitHub',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://github.com/JayantTaneja", "_blank");
         },
       },{
       id: 'light-theme',
